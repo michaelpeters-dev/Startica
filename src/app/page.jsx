307479fded0249a1f996fb6ce33758483a25c9d7
michaelcpeters115 <123867234+michaelcpeters115@users.noreply.github.com>
2025-07-09 12:51:00 +0200

    Initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="bg-black min-h-screen  flex max-w-[1500px]  mx-auto">
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Feed */}
+        {/* Widgets */}
+        {/* Modal */}
+      </main>
+    </div>
+  );
+}

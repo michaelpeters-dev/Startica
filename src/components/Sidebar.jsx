2b0693f75d4cdba60053b96e47266f4e62595065
michaelcpeters115 <123867234+michaelcpeters115@users.noreply.github.com>
2025-07-10 17:17:30 +0200

    Implement initial Sidebar feature

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,58 @@
+import Image from "next/image";
+import SidebarLink from "./SidebarLink";
+import {
+  Home,
+  Search,
+  Bell,
+  Inbox,
+  Bookmark,
+  ClipboardList,
+  List,
+  User,
+  MoreHorizontal,
+} from "lucide-react";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+      <div className="w-full xl:ml-24">
+        <Image
+          src="/plain_name.png"
+          alt="Startica Wording"
+          width={10}
+          height={0}
+          sizes="100vw"
+          style={{ width: "45%", height: "auto" }}
+          className="hoverAnimation mt-3"
+        />
+      </div>
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+        <SidebarLink Icon={Home} text="Home" active />
+        <SidebarLink Icon={Search} text="Explore" />
+        <SidebarLink Icon={Bell} text="Notifications" />
+        <SidebarLink Icon={Inbox} text="Messages" />
+        <SidebarLink Icon={Bookmark} text="Bookmarks" />
+        <SidebarLink Icon={ClipboardList} text="Lists" />
+        <SidebarLink Icon={User} text="Profile" />
+        <SidebarLink Icon={MoreHorizontal} text="More" />
+      </div>
+      <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
+        Post
+      </button>
+      <div className="text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:ml-auto xl:-mr-5 mt-auto">
+        <img
+          src="/circular_border.png"
+          alt="User profile image"
+          className="h-10 w-10 rounded-full xl:mr-2.5"
+        />
+        <div className="hidden xl:inline leading-5">
+          <h4 className="font-bold">firebase 18774</h4>
+          <p className="text-[#6e767d]">@firebase1875</p>
+        </div>
+        <MoreHorizontal className="h-5 hidden xl:inline ml-10" />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
